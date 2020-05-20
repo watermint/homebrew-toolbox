5b16760bfce72be765c9b8dbd94af6202396135a
takayuki <watermint@users.noreply.github.com>
2020-05-20 17:59:07 +0900

    brew audit

--- toolbox.rb
+++ toolbox.rb
@@ -1,7 +1,6 @@
-
 class Toolbox < Formula
   desc "CLI tools for Dropbox and Dropbox Business"
-  homepage ""
+  homepage "https://github.com/watermint/toolbox"
   url "https://github.com/watermint/toolbox/releases/download/67.4.414/tbx-67.4.414-mac.zip"
   sha256 "9924ff9e2bce39c1eb5eaa2ca1d239698f53e8eb59044cfaf19d7a006ce1db82"
 
