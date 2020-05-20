e0a29386a160a5cc6fd06c66eea250738e99f7b5
takayuki <watermint@users.noreply.github.com>
2020-05-20 21:11:38 +0900

    remove bottle

--- toolbox.rb
+++ toolbox.rb
@@ -4,8 +4,6 @@ class Toolbox < Formula
   url "https://github.com/watermint/toolbox/releases/download/67.4.414/tbx-67.4.414-mac.zip"
   sha256 "9924ff9e2bce39c1eb5eaa2ca1d239698f53e8eb59044cfaf19d7a006ce1db82"
 
-  bottle :unneeded
-
   def install
     bin.install "tbx"
   end
