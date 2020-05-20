183a64184eb4831958cada66f36d0ee71e7d5c97
takayuki <watermint@users.noreply.github.com>
2020-05-20 18:20:29 +0900

    update

--- toolbox.rb
+++ toolbox.rb
@@ -4,11 +4,9 @@ class Toolbox < Formula
   url "https://github.com/watermint/toolbox/releases/download/67.4.414/tbx-67.4.414-mac.zip"
   sha256 "9924ff9e2bce39c1eb5eaa2ca1d239698f53e8eb59044cfaf19d7a006ce1db82"
 
+  bottle :unneeded
+
   def install
     bin.install "tbx"
-  end
-
-  test do
-    tbx version
   end
 end
