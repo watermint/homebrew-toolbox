d9ec55d0b0297ea4e3f09cf9e6c1af95dfca83df
Takayuki Okazaki <watermint@users.noreply.github.com>
2020-05-24 15:00:51 +0900

    Update toolbox.rb

--- toolbox.rb
+++ toolbox.rb
@@ -1,7 +1,7 @@
 class Toolbox < Formula
   desc "CLI tools for Dropbox and Dropbox Business"
   homepage "https://github.com/watermint/toolbox"
-  url "https://github.com/watermint/toolbox/releases/download/untagged-2aeb8a07cb30e17c2c5e/tbx-68.4.428-mac.zip"
+  url "https://github.com/watermint/toolbox/releases/download/68.4.428/tbx-68.4.428-mac.zip"
   sha256 "175f2df430dfc784f719d7e6c101ce89fb73b98898f8a856f621e8d0d3b8f2c0"
 
   def install
