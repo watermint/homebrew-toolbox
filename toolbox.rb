6ab97442d673ebe145dac5aec2ed2cce6a5c1379
Takayuki Okazaki <watermint@users.noreply.github.com>
2021-10-09 21:31:27 +0900

    Release 98.8.1112

--- toolbox.rb
+++ toolbox.rb
@@ -1,8 +1,8 @@
 class Toolbox < Formula
   desc "The multi-purpose utility command-line tool for web services including Dropbox, Dropbox Business, Google, GitHub, etc."
   homepage "https://github.com/watermint/toolbox"
-  url "https://github.com/watermint/toolbox/releases/download/97.8.1103/tbx-97.8.1103-mac.zip"
-  sha256 "16f76d4092bb5dee1f61f0b94a475da2793327a5d9b23640caf8378591612fef"
+  url "https://github.com/watermint/toolbox/releases/download/98.8.1112/tbx-98.8.1112-mac-arm.zip"
+  sha256 "92765af09bf6b298c45950623ce4c447700949deb36bfd57cef9a3fb918e9164"
 
   def install
     bin.install "tbx"
