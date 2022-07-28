12fe7ef74734b1d2018519575a8c82b600e68b31
Takayuki Okazaki <watermint@users.noreply.github.com>
2022-07-28 15:35:24 +0900

    fix syntax

--- toolbox.rb
+++ toolbox.rb
@@ -13,7 +13,8 @@ class Toolbox < Formula
     on_arm do
       url    "https://github.com/watermint/toolbox/releases/download/untagged-c7d1cf05baa9d676dbaa/tbx-105.8.1229-mac-arm.zip"
       sha256 "8dad1a060ce22103f45f3b6bef9283c65a305a409fd37757b8fcb64da4bd86dc"
-    else
+    end
+    on_intel do
       url    "https://github.com/watermint/toolbox/releases/download/untagged-c7d1cf05baa9d676dbaa/tbx-105.8.1229-mac.zip"
       sha256 "754f8b531d599710cda9acc6aabf271352d98bd4e95ba99b192c5ae76b73ab5c"
     end
