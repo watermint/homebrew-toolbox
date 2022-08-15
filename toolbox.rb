5c4032eca5c35de93ba49d9730a33778e9ad7bef
Takayuki Okazaki <watermint@users.noreply.github.com>
2022-08-15 16:10:31 +0900

    Release 108.8.729

--- toolbox.rb
+++ toolbox.rb
@@ -1,28 +1,28 @@
 class Toolbox < Formula
   desc "The multi-purpose utility command-line tool for web services including Dropbox, Dropbox Business, Google, GitHub, etc."
   homepage "https://github.com/watermint/toolbox"
-  version "107.8.720"
+  version "108.8.729"
   license "MIT"
 
   on_linux do
     on_arm do
-      url    "https://github.com/watermint/toolbox/releases/download/107.8.720/tbx-107.8.720-linux-arm.zip"
-      sha256 "72af25dcbc20827e697070dccccd226caec21842f9a1f3d7d84fadeca0c49460"
+      url    "https://github.com/watermint/toolbox/releases/download/108.8.729/tbx-108.8.729-linux-arm.zip"
+      sha256 "95cb4eec8393ace47d884c41da25b0be401dbd9efb4a8f9837d16d918a4886c6"
     end
     on_intel do
-      url    "https://github.com/watermint/toolbox/releases/download/107.8.720/tbx-107.8.720-linux.zip"
-      sha256 "6e5153f81533d68e71c7f44fd4182132841b2215aa161f1e253a160c7280fd00"
+      url    "https://github.com/watermint/toolbox/releases/download/108.8.729/tbx-108.8.729-linux.zip"
+      sha256 "87e8629390ef236764cf71726974ce444570534491389e14be56617ba0224176"
     end
   end
 
   on_macos do
     on_arm do
-      url    "https://github.com/watermint/toolbox/releases/download/107.8.720/tbx-107.8.720-mac-arm.zip"
-      sha256 "74085c6de7b3fcd5c7502b89321414e04ffb3def52ac2372a8bf7095f2a69365"
+      url    "https://github.com/watermint/toolbox/releases/download/108.8.729/tbx-108.8.729-mac-arm.zip"
+      sha256 "64ae76b2d2c1d6b2e0439721165839154da6bea53999794fdc1243eda96b4f1b"
     end
     on_intel do
-      url    "https://github.com/watermint/toolbox/releases/download/107.8.720/tbx-107.8.720-mac.zip"
-      sha256 "21ba034b591692195d806c3ba239081d988141024331b324fcb8b8873ec271c4"
+      url    "https://github.com/watermint/toolbox/releases/download/108.8.729/tbx-108.8.729-mac.zip"
+      sha256 "0106f05cded1b7a8d188ac0fa92ce936f6e82eb9fe7dd7a4a87a41a8403e60b5"
     end
   end
 
