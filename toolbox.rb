3270839096c2d3bbc625355b452b6ea8cfb67935
Takayuki Okazaki <watermint@users.noreply.github.com>
2023-12-06 22:14:46 +0900

    Release 124.8.103

--- toolbox.rb
+++ toolbox.rb
@@ -1,28 +1,28 @@
 class Toolbox < Formula
   desc "The multi-purpose utility command-line tool for web services including Dropbox, Dropbox Business, Google, GitHub, etc."
   homepage "https://github.com/watermint/toolbox"
-  version "124.8.101"
+  version "124.8.103"
   license "MIT"
 
   on_linux do
     on_arm do
-      url    "https://github.com/watermint/toolbox/releases/download/124.8.101/tbx-124.8.101-linux-arm.zip"
-      sha256 "bfd04fdfa788a69bdc9240497821b5a3f138b643d7a7bef350f836f8fc5656ca"
+      url    "https://github.com/watermint/toolbox/releases/download/124.8.103/tbx-124.8.103-linux-arm.zip"
+      sha256 "80148a611f0578292cc78687900631e4e9f6d51bf319282e0dbdfb6be1161a28"
     end
     on_intel do
-      url    "https://github.com/watermint/toolbox/releases/download/124.8.101/tbx-124.8.101-linux-intel.zip"
-      sha256 "47b4ebb85d97635bb511f610972aa0706c18fa3126e80bc6e8e44cd0eec55061"
+      url    "https://github.com/watermint/toolbox/releases/download/124.8.103/tbx-124.8.103-linux-intel.zip"
+      sha256 "38e1c97039cbdc7586ab72649a8a596079310f64cf480850f167f6ee52a83322"
     end
   end
 
   on_macos do
     on_arm do
-      url    "https://github.com/watermint/toolbox/releases/download/124.8.101/tbx-124.8.101-mac-applesilicon.zip"
-      sha256 "ce26fd0cf898923c5a86eaffe64e38396152f2d809f62f80ea8c98c2f698173c"
+      url    "https://github.com/watermint/toolbox/releases/download/124.8.103/tbx-124.8.103-mac-applesilicon.zip"
+      sha256 "e655be4d27d45a605d475041b9be28338f69a96b37d0ae94661efe3879dd1d1f"
     end
     on_intel do
-      url    "https://github.com/watermint/toolbox/releases/download/124.8.101/tbx-124.8.101-mac-intel.zip"
-      sha256 "8e5f15cccf5bc29fee886ffce2984f821232c12a334c511cb478a68ee3468baa"
+      url    "https://github.com/watermint/toolbox/releases/download/124.8.103/tbx-124.8.103-mac-intel.zip"
+      sha256 "10f690cabc8e9e78a9268afd5f0db643f35b670743a73534ce385d970a50ceee"
     end
   end
 
