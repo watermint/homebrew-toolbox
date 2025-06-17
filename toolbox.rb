1e7713a2b4dd35f0b3164825ff1e8cf9486aa802
Takayuki Okazaki <watermint@users.noreply.github.com>
2025-06-17 21:22:58 +0900

    Release 142.8.328

--- toolbox.rb
+++ toolbox.rb
@@ -1,24 +1,24 @@
 class Toolbox < Formula
   desc "The multi-purpose utility command-line tool for web services including Dropbox, Dropbox Business, Google, GitHub, etc."
   homepage "https://github.com/watermint/toolbox"
-  version "141.8.323"
+  version "142.8.328"
   license "MIT"
 
   on_linux do
     on_arm do
-      url    "https://github.com/watermint/toolbox/releases/download/141.8.323/tbx-141.8.323-linux-arm.zip"
-      sha256 "15955adfa2800b734e29b506e62f489ecdd6d4b9f05eb935577860b1ba30e9ae"
+      url    "https://github.com/watermint/toolbox/releases/download/142.8.328/tbx-142.8.328-linux-arm.zip"
+      sha256 "3bc3f0905f641f7feb74d1cc2e7363a3376fb0af368be39d29c5552ced7adc23"
     end
     on_intel do
-      url    "https://github.com/watermint/toolbox/releases/download/141.8.323/tbx-141.8.323-linux-intel.zip"
-      sha256 "d64bace139787090460f79fa67220134b38f57d25438dfb60eea421e2a78d435"
+      url    "https://github.com/watermint/toolbox/releases/download/142.8.328/tbx-142.8.328-linux-intel.zip"
+      sha256 "12690b433e09f08c06f871bbf4f52b848ad35541312cf0e00b73942e79069850"
     end
   end
 
   on_macos do
     on_arm do
-      url    "https://github.com/watermint/toolbox/releases/download/141.8.323/tbx-141.8.323-mac-applesilicon.zip"
-      sha256 "04901663733b9d88acb38a098893f76374aaa4163f4c8e08b93e9b899316a092"
+      url    "https://github.com/watermint/toolbox/releases/download/142.8.328/tbx-142.8.328-mac-applesilicon.zip"
+      sha256 "f90b2a23d2fbcd5cc9900df37b711b8caf1bb17e8395c45688d31c4c85b9c7f0"
     end
     on_intel do
       url    "<no value>"
